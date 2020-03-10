c66e29284d67b771055790e2f25f51c2e767c57f
Jacob Langdon <62000639+Jplangdon@users.noreply.github.com>
2020-03-09 23:10:57 -0400

    Initial commit

---
+++ Collection tutorial/FileParser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection_tutorial
+{
+    class FileParser : IFileParser
+    {
+       public string[] Read(string path)
+        {
+
+            var reader = new StreamReader(path);
+
+           int arraySize = int.Parse(reader.ReadLine());
+
+           string[] array = new string[arraySize];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = reader.ReadLine();
+            }
+
+            reader.Close();
+
+            reader.Dispose();
+
+            return array;                
+        }
+    }
+}
---
+++ Collection tutorial/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Collection_tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IFileParser varible = new FileParser();
+
+            var data = parser.Read("C:/Users/jlang/Documents/jacob/C#/data.txt"); C: \Users\jlang\Documents\jacob\C#
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                Console.WriteLine(data[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
